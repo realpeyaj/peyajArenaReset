68999cc8944bb4ebb1064d788b2435cbb641a54e
realpeyaj <princejohnguadz271@gmail.com>
2026-02-20 22:49:04 +0800

    Remove ps1 and txt scripts from project

--- fix_package.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem -Path . -Recurse -Include *.java,*.yml,*.xml | Where-Object { $_.FullName -notmatch '\\target\\' -and $_.FullName -notmatch '\\.git\\' -and $_.FullName -notmatch '\\.gemini\\' }
-foreach ($file in $files) {
-    $content = [System.IO.File]::ReadAllText($file.FullName)
-    $newContent = $content -cReplace 'com\.peyaj\.peyajarenareset', 'com.peyaj.arenareset'
-    if ($content -cne $newContent) {
-        [System.IO.File]::WriteAllText($file.FullName, $newContent)
-    }
-}
